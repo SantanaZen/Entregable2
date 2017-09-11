6bc124f0328a39d7718a3b069cdce03d01ddaaf0
SantanaZen <danji_9334@hotmail.com>
2017-09-10 21:57:28 -0500

    primer commit

---
+++ app/src/main/java/com/example/dell/entragaleria/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.dell.entragaleria;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private  final int SELECT_PICTURE=300;
+    private ImageView imagen;
+    private Button boton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imagen=(ImageView)findViewById(R.id.mirame);
+        boton=(Button)findViewById(R.id.pinchame);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Selecciona"), SELECT_PICTURE);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK) {
+            Uri path = data.getData();
+            imagen.setImageURI(path);
+        }
+    }
+
+
+}
